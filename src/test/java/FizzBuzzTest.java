fdaedc271f3a496cc482d2e7809b3c9dad18e1f7
Chemister1 <alfa4748@gmail.com>
2022-11-19 21:14:59 +0800

    fourth commmit

--- src/test/java/FizzBuzzTest.java
+++ src/test/java/FizzBuzzTest.java
@@ -12,7 +12,7 @@ public class FizzBuzzTest {
         //Arrange
         int start =1;
         int end = 20;
-        String[] expectedResult = {"1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz",
+        String[] expectedResult = {"1","2","Fizz"," 4","Buzz","Fizz","7","8","Fizz","Buzz","11","Fizz","13","14","FizzBuzz",
             "16","17","Fizz","19","Buzz"};
 
         //Act
